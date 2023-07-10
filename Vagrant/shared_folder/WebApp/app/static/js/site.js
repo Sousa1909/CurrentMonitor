2b7c31d25bb292cad156f341301d02cb763b8782
João <jogui4@hotmail.com>
2023-07-10 22:24:33 +0100

    página web quase concluída

---
+++ Vagrant/shared_folder/WebApp/app/static/js/site.js
@@ -0,0 +1,105 @@
+// Exemplo de dados recebidos do sensor
+const dadosRecebidos = [10, 15, 20, 25, 30];
+
+// Configuração inicial do gráfico
+const config = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Dados do Sensor',
+            data: [],
+            backgroundColor: 'rgba(93, 155, 212, 0.2)',
+            borderColor: 'rgba(93, 155, 212, 1)',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            x: {
+                display: true,
+                title: {
+                    display: true,
+                    text: 'Tempo'
+                }
+            },
+            y: {
+                display: true,
+                title: {
+                    display: true,
+                    text: 'Valor'
+                }
+            }
+        }
+    }
+};
+
+// Criação do gráfico
+const ctx = document.getElementById('grafico').getContext('2d');
+const chart = new Chart(ctx, config);
+
+
+// Variável para armazenar o valor atual
+let valorAtual = null;
+
+const historicoValores = [];
+
+const taxaCustoPorAmpere = 0.05;
+
+// Função para calcular o custo com base nos amperes
+function calcularCusto(amperes) {
+    return amperes * taxaCustoPorAmpere;
+}
+
+function calcularCustoTotal(historico) {
+    let custoTotal = 0;
+    for (let i = 0; i < historico.length; i++) {
+        custoTotal += calcularCusto(historico[i]);
+    }
+    return custoTotal;
+}
+
+// Função para verificar e atualizar o estado do botão de alerta
+function verificarAlerta(valor) {
+    const btnAlert = document.getElementById('condições').querySelector('#btnAlert');
+    if (valor > 10) {
+        console.log('Valor maior que 10');
+        btnAlert.classList.add('piscar');
+    } else {
+        console.log('Valor menor que 10');
+        btnAlert.classList.remove('piscar');
+    }
+}
+
+// Função para atualizar o gráfico com os novos dados
+function atualizarGrafico(dados) {
+    chart.data.labels.push(new Date().toLocaleTimeString()); 
+    chart.data.datasets[0].data.push(dados); // Adicionar o novo dado
+    chart.update(); // Atualizar o gráfico
+
+    historicoValores.push(dados);
+
+    valorAtual = dados;
+    document.getElementById('informacao1-valor').textContent = valorAtual;
+
+    // Calcular o custo com base nos amperes
+    const custo = calcularCusto(dados);
+    // Atualizar o valor exibido na seção "Informação 2"
+    document.getElementById('informacao2-valor').textContent = custo.toFixed(2); // Exibir com duas casas decimais
+
+    // Calcular o custo total com base no histórico de valores
+    const custoTotal = calcularCustoTotal(historicoValores);
+    // Atualizar o valor exibido na secção "Custo Total"
+    document.getElementById('custo-total-valor').textContent = custoTotal.toFixed(2); // Exibir com duas casas decimais
+
+    // Verificar o alerta com base no último valor recebido
+    const ultimoValor = dados[dados.length - 1];
+    verificarAlerta(ultimoValor);
+}
+
+// Simulação de recebimento contínuo de dados do sensor
+setInterval(() => {
+    const novoDado = Math.random() * 100; // Gerar um novo dado aleatório
+    atualizarGrafico(novoDado);
+}, 1000); // Atualizar a cada segundo
